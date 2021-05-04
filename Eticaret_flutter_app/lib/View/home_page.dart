28519d9dbe711c9fde3fd7b4854cf99a729bfd44
Bünyamin Ertas <79013103+ertasbunyamin@users.noreply.github.com>
2021-05-04 15:59:09 +0300

    Search done in homepage!

--- Eticaret_flutter_app/lib/View/home_page.dart
+++ Eticaret_flutter_app/lib/View/home_page.dart
@@ -11,45 +11,74 @@ import '../Map/main_drawer.dart';
 import '../DesignStyle/colors_cons.dart';
 
 //anasayfa
-
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   static String routeName = '/routeHomePage';
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  TextEditingController controller = new TextEditingController();
+
   void _onTileClicked(Product snapshot,var context){
     Navigator.push(context, MaterialPageRoute(
-        builder: (context) => AdDetail(snapshot: snapshot,),
+      builder: (context) => AdDetail(snapshot: snapshot,),
     ));
+  }
+  onSearchTextChanged(String text) async {
+    _searchResult.clear();
+    if (text.isEmpty) {
+      setState(() {});
+      return;
+    }
+
+    _products.forEach((product) {
+      if (product.productTitle.contains(text) || product.productOfDescription.contains(text)
+          || product.productCategory.contains(text))
+        _searchResult.add(product);
+    });
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
     const double _radius = 3;
     ProductShareService _productShareService=ProductShareService();
-    final searchInput = TextField(
-      cursorColor: searchText,
-      style: TextStyle(
-        color: searchText,
-        fontWeight: FontWeight.bold,
+    final searchInput = new ListTile(
+      leading: new Icon(Icons.search),
+      title: new TextField(
+        controller: controller,
+        style: TextStyle(
+          color: searchText,
+          fontWeight: FontWeight.bold,
+        ),
+        decoration: InputDecoration(
+          contentPadding: EdgeInsets.only(left: 8),
+          hintText: "Ürün Ara",
+          hintStyle: TextStyle(color: searchTextHint),
+          filled: true,
+          fillColor: themeColor,
+          enabledBorder: UnderlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(_radius)),
+            borderSide: BorderSide(color: themeColor),
+          ),
+          focusedBorder: UnderlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(_radius)),
+            borderSide: BorderSide(color: themeColor),
+          ),
+          border: UnderlineInputBorder(
+            borderRadius: BorderRadius.all(Radius.circular(_radius)),
+            borderSide: BorderSide(color: themeColor),
+          ),
+        ),
+        onChanged: onSearchTextChanged,
       ),
-      decoration: InputDecoration(
-        contentPadding: EdgeInsets.only(left: 8),
-        hintText: "Ürün Ara",
-        hintStyle: TextStyle(color: searchTextHint),
-        filled: true,
-        fillColor: themeColor,
-        enabledBorder: UnderlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(_radius)),
-          borderSide: BorderSide(color: themeColor),
-        ),
-        focusedBorder: UnderlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(_radius)),
-          borderSide: BorderSide(color: themeColor),
-        ),
-        border: UnderlineInputBorder(
-          borderRadius: BorderRadius.all(Radius.circular(_radius)),
-          borderSide: BorderSide(color: themeColor),
-        ),
-      ),
+      trailing: new IconButton(icon: new Icon(Icons.cancel), onPressed: () {
+        controller.clear();
+        onSearchTextChanged('');
+      },),
     );
+
     return Scaffold(
       backgroundColor: background,
       resizeToAvoidBottomInset: false,
@@ -70,120 +99,116 @@ class HomePage extends StatelessWidget {
       ),
       drawer: MainDrawer(),
       body: StreamBuilder(
-        stream:_productShareService.getProduct(),
-        builder:(BuildContext context,AsyncSnapshot<List<Product>> snapshot) {
-          if(snapshot.hasError){
-            return Text(snapshot.error.toString());
-          }
-          if(snapshot.connectionState==ConnectionState.waiting){
-            return Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          if (snapshot.hasData && snapshot.data.isEmpty) {
-            return Text("Document does not exist");
-          }
-
-          return ListView.builder(
-              itemCount: snapshot.data.length,
-              itemBuilder: (context, index) {
-                return Container(
-                  child: InkResponse(
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Container(
-                        height: MediaQuery
-                            .of(context)
-                            .size
-                            .height / 2,
-                        width: MediaQuery
-                            .of(context)
-                            .size
-                            .width,
-                        decoration: BoxDecoration(
-                          color: filterBackground,
-                          borderRadius: BorderRadius.all(Radius.circular(12)),
-                        ),
-                        child: Column(
-                          children: [
-                            Expanded(
-                              flex: 5,
-                              child: Container(
-                                width: double.maxFinite,
-                                height: double.maxFinite,
-                                child: FittedBox(
-                                  fit: BoxFit.fill,
-                                  child: snapshot.data.elementAt(index).productImage1 == "" ?
-                                    Image.asset("images/Opel_KARL.jpg") : Image.network(snapshot.data.elementAt(index).productImage1),
+          stream:_productShareService.getProduct(),
+          builder:(BuildContext context,AsyncSnapshot<List<Product>> snapshot) {
+            _products = snapshot.data;
+            if(snapshot.hasError){
+              return Text(snapshot.error.toString());
+            }
+            if(snapshot.connectionState==ConnectionState.waiting){
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.hasData && snapshot.data.isEmpty) {
+              return Text("Document does not exist");
+            }
+            return ListView.builder(
+                itemCount: _searchResult.isNotEmpty ? _searchResult.length:snapshot.data.length,
+                itemBuilder: (context, index) {
+                  Size size = MediaQuery.of(context).size;
+                  return Container(
+                    child: InkResponse(
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Container(
+                          height: size.height / 2,
+                          width: size.width,
+                          decoration: BoxDecoration(
+                            color: filterBackground,
+                            borderRadius: BorderRadius.all(Radius.circular(12)),
+                          ),
+                          child: Column(
+                            children: [
+                              Expanded(
+                                flex: 5,
+                                child: Container(
+                                  width: double.maxFinite,
+                                  height: double.maxFinite,
+                                  child: FittedBox(
+                                    fit: BoxFit.fill,
+                                    child: _searchResult.isNotEmpty? isEmptyImage(_searchResult[index])
+                                    :isEmptyImage(snapshot.data.elementAt(index)),
+                                  ),
                                 ),
                               ),
-                              ),
 
-                            Expanded(
-                              flex: 1,
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Expanded(
-                                    flex: 1,
-                                    child: Padding(
-                                      padding: const EdgeInsets.only(
-                                          left: 15.0),
-                                      child: Text(
-                                        "${snapshot.data.elementAt(index).productTitle}",
-                                        textAlign: TextAlign.start,
-                                        style: TextStyle(color: text,
-                                          fontSize: 18,
-                                          fontFamily: 'Tienne',
-                                          fontWeight: FontWeight.bold,),
+                              Expanded(
+                                flex: 1,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  crossAxisAlignment: CrossAxisAlignment.center,
+                                  children: [
+                                    Expanded(
+                                      flex: 1,
+                                      child: Padding(
+                                        padding: const EdgeInsets.only(
+                                            left: 15.0),
+                                        child: Text(_searchResult.isNotEmpty?"${_searchResult[index].productTitle}":
+                                          "${snapshot.data.elementAt(index).productTitle}",
+                                          textAlign: TextAlign.start,
+                                          style: TextStyle(color: text,
+                                            fontSize: 18,
+                                            fontFamily: 'Tienne',
+                                            fontWeight: FontWeight.bold,),
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                  Expanded(
-                                    flex: 1,
-                                    child: Column(
-                                      mainAxisAlignment: MainAxisAlignment
-                                          .center,
-                                      crossAxisAlignment: CrossAxisAlignment
-                                          .center,
-                                      children: [
-                                        Text("43.500 TL",
-                                          style: TextStyle(
+                                    Expanded(
+                                      flex: 1,
+                                      child: Column(
+                                        mainAxisAlignment: MainAxisAlignment
+                                            .center,
+                                        crossAxisAlignment: CrossAxisAlignment
+                                            .center,
+                                        children: [
+                                          Text("43.500 TL",
+                                            style: TextStyle(
+                                                color: themeColor,
+                                                fontFamily: 'Tienne',
+                                                fontSize: 15,
+                                                fontWeight: FontWeight.bold,
+                                                decoration: TextDecoration
+                                                    .lineThrough
+                                            ),
+                                          ),
+                                          Text(_searchResult.isNotEmpty?"${_searchResult[index].productTitle}":
+                                            "${snapshot.data.elementAt(index).productPrice}",
+                                            style: TextStyle(
                                               color: themeColor,
+                                              fontSize: 25,
                                               fontFamily: 'Tienne',
-                                              fontSize: 15,
                                               fontWeight: FontWeight.bold,
-                                              decoration: TextDecoration
-                                                  .lineThrough
+                                            ),
                                           ),
-                                        ),
-                                        Text("${snapshot.data.elementAt(index).productPrice}",
-                                          style: TextStyle(
-                                            color: themeColor,
-                                            fontSize: 25,
-                                            fontFamily: 'Tienne',
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                      ],
+                                        ],
+                                      ),
                                     ),
-                                  ),
-                                ],
+                                  ],
+                                ),
                               ),
-                            ),
-                          ],
+                            ],
+                          ),
                         ),
                       ),
+                      onTap: () => _onTileClicked(snapshot.data.elementAt(index), context),
                     ),
-                    onTap: () => _onTileClicked(snapshot.data.elementAt(index), context),
-                  ),
-                );
-              }
+                  );
+                }
 
 
-          );
-        }
+            );
+          }
       ),
 
       floatingActionButton: FloatingActionButton(
@@ -202,4 +227,11 @@ class HomePage extends StatelessWidget {
       ),
     );
   }
+
+  Widget isEmptyImage(Product product)=>product.productImage1 == "" ?
+  Image.asset("images/Opel_KARL.jpg") : Image.network(product.productImage1);
+
+  List<Product> _searchResult = [];
+
+  List<Product> _products = [];
 }
